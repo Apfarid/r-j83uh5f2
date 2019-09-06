50132922136ea7c11fbb1cd25aabf9c58bfbfa95
apfarid <apfarid780@gmail.com>
2019-09-05 21:35:44 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    numero : 0
+  }
+
+  contar(){
+    this.setState({
+      numero : this.state.numero + 1
+    })
+  }
+
+
+
+
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onKeyPress = {this.contar.bind(this)}></textarea>
+        <div className="counter">{this.state.numero}</div>
       </div>
     );
   }
